d90ba6f1b171e8f6d6aca0616807404f7af88464
Michael Hunt <mhunt429@gmail.com>
2025-01-11 11:22:39 -0500

    Fixing the build

--- src/Selah.Infrastructure/Repository/Interfaces/IFinancialAccountRepository.cs
+++ src/Selah.Infrastructure/Repository/Interfaces/IFinancialAccountRepository.cs
@@ -4,14 +4,15 @@ namespace Selah.Infrastructure.Repository;
 
 public interface IFinancialAccountRepository
 {
-    Task <bool> ImportFinancialAccountsAsync(IEnumerable<FinancialAccountSqlInsert> accounts);
-   
-    Task<long>  AddAccountAsync(FinancialAccountSqlInsert account);
-    
+    Task<bool> ImportFinancialAccountsAsync(IEnumerable<FinancialAccountSqlInsert> accounts);
+
+    Task<long> AddAccountAsync(FinancialAccountSqlInsert account);
+
     Task<IEnumerable<FinancialAccountSql>> GetAccountsAsync(Guid userId);
-    
+
     Task<FinancialAccountSql> GetAccountByIdAsync(Guid userId, long id);
-    
+
     Task<bool> UpdateAccountBalance(FinancialAccountBalanceUpdate accountBalanceUpdate, Guid userId);
-    
+
     Task<bool> DeleteAccountAsync(Guid userId, long id);
+}
